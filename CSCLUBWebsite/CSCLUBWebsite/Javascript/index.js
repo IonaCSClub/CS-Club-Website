339fc9b2e9e3107e267cebab6e594c06e7b9553b
NexusVMaster <90575967+NexusVMaster@users.noreply.github.com>
2023-10-13 17:13:18 -0400

    Update index.js

--- CSCLUBWebsite/CSCLUBWebsite/Javascript/index.js
+++ CSCLUBWebsite/CSCLUBWebsite/Javascript/index.js
@@ -1,1 +1,2 @@
+document.getElementById("test_me").innerHTML +="<a href='index.html'>Home</a> <a href='#about'>About Us</a><a href='#events'>Events</a><a href='#join'>Join Us</a><a href='Currentmembers.html'>Current Members and Alumni</a><a href='#contact'>Contact</a>";
 
