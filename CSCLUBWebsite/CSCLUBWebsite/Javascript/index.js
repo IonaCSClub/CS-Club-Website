6661137e2e038351ec29a811f03a67ed5aec746c
Adonis Paul <padonis007@gmail.com>
2023-10-13 23:45:01 -0400

    Updated JS anchor hrefs

--- CSCLUBWebsite/CSCLUBWebsite/Javascript/index.js
+++ CSCLUBWebsite/CSCLUBWebsite/Javascript/index.js
@@ -1,2 +1,2 @@
-document.getElementById("test_me").innerHTML +="<a href='index.html'>Home</a> <a href='#about'>About Us</a><a href='#events'>Events</a><a href='#join'>Join Us</a><a href='Currentmembers.html'>Current Members and Alumni</a><a href='#contact'>Contact</a>";
+document.getElementById("test_me").innerHTML +="<a href='index.html'>Home</a> <a href='aboutus.html'>About Us</a><a href='events.html'>Events</a><a href='join.html'>Join Us</a><a href='Aboutpresidents.html'>Current Members and Alumni</a><a href='contact.html'>Contact</a>";
 
