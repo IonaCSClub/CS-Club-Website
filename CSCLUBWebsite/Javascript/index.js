9d58b3851009ba7d46d39644ef21b4a2f3d972cd
Adonis Paul <padonis007@gmail.com>
2023-10-14 00:24:49 -0400

    Removed redundant folder directory

---
+++ CSCLUBWebsite/Javascript/index.js
@@ -0,0 +1,2 @@
+document.getElementById("test_me").innerHTML +="<a href='index.html'>Home</a> <a href='aboutus.html'>About Us</a><a href='events.html'>Events</a><a href='join.html'>Join Us</a><a href='Aboutpresidents.html'>Current Members and Alumni</a><a href='contact.html'>Contact</a>";
+
