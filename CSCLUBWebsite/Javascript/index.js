986c51b81186bacf7689708db6654ac5c598c12e
NexusVMaster <tavisajohnson@gmail.com>
2023-10-16 12:03:49 -0400

    updated nav bar and made new page and removed group member page

--- CSCLUBWebsite/Javascript/index.js
+++ CSCLUBWebsite/Javascript/index.js
@@ -1,2 +1,2 @@
-document.getElementById("test_me").innerHTML +="<a href='index.html'>Home</a> <a href='aboutus.html'>About Us</a><a href='events.html'>Events</a><a href='Aboutpresidents.html'>Current Members and Alumni</a><a href='join.html'>Join Us</a>";
+document.getElementById("test_me").innerHTML +="<a href='index.html'>Home</a> <a href='aboutUs.html'>About Us</a><a href='events.html'>Events</a><a href='Aboutpresidents.html'>Current Members and Alumni</a><a href='join.html'>Join Us</a><a href='projects.html'>Projects</a>";
 
